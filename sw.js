10bf02ab9ffaad9ee2c293a8eb42070e8f8e6eef
MEffinDoC <mdsdoc1@gmail.com>
2025-10-07 19:17:24 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// YDB sw.js v15b
-const SCOPE='/YourDamnBudget/', CACHE='ydb-v15b';
-const ASSETS=[`${SCOPE}`,`${SCOPE}index.html`,`${SCOPE}styles.css?v=15`,`${SCOPE}app.js?v=15.2`,`${SCOPE}engine.js`,`${SCOPE}wizard.js?v=15.2`,`${SCOPE}manifest.webmanifest`,`${SCOPE}icons/flat-192.png`,`${SCOPE}icons/flat-512.png`];
+// YDB sw.js v15c
+const SCOPE='/YourDamnBudget/', CACHE='ydb-v15c';
+const ASSETS=[`${SCOPE}`,`${SCOPE}index.html`,`${SCOPE}styles.css?v=15.3`,`${SCOPE}app.js?v=15.3`,`${SCOPE}engine.js`,`${SCOPE}wizard.js?v=15.2`,`${SCOPE}manifest.webmanifest`,`${SCOPE}icons/flat-192.png`,`${SCOPE}icons/flat-512.png`];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{const u=new URL(e.request.url); if(!u.pathname.startsWith(SCOPE)) return;
