de52085f4cf59a726f41dddbbe310f07fa4c9e0f
MEffinDoC <mdsdoc1@gmail.com>
2025-10-11 01:27:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-// YDB LKG SW (very light)
-const CACHE = 'ydb-lkg-15.8';
+// YDB SW – v15.9.0 (LKG)
+const CACHE = 'ydb-v15m';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=15.8-lkg',
-  './app.js?v=15.8-lkg',
-  './manifest.webmanifest'
+  './styles.css?v=15.9.0',
+  './app.js?v=15.9.0',
+  './manifest.webmanifest',
+  './icons/flat-192.png',
+  './icons/flat-512.png'
 ];
 
 self.addEventListener('install', e=>{
@@ -13,7 +15,9 @@ self.addEventListener('install', e=>{
   self.skipWaiting();
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e=>{
